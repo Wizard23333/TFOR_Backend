c64978058b5d84c15362e075b7840d4c94d092e5
Haotian Wu <3233990645@qq.com>
2021-12-05 14:29:42 +0800

    add project basic structure

---
+++ src/main/java/com/tongji/tfor_backend/configuration/AppConfiguration.java
@@ -0,0 +1,24 @@
+package com.tongji.tfor_backend.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfiguration {
+    // 生成一个一个bean
+    // 配置所有的访问接口
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        // 嵌套类
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                // 允许所有的ip访问
+                registry.addMapping("/**")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+}
