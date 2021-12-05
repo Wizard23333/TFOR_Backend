e6c6eb397c2f0820c114085dc917ffb8f41d15fc
Haotian Wu <3233990645@qq.com>
2021-12-05 14:29:42 +0800

    add project basic structure

---
+++ src/main/java/com/tongji/tfor_backend/TforBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tongji.tfor_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TforBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TforBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tongji/tfor_backend/TforBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tongji.tfor_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TforBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
