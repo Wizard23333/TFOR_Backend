afa55d77740e3044202dd3a08e3c214638b96e9a
Haotian Wu <3233990645@qq.com>
2021-12-08 21:26:51 +0800

    simple demo for a api

---
+++ src/main/java/cn/edu/tongji/tfor_backend/controller/ZoneViewController.java
@@ -0,0 +1,33 @@
+package cn.edu.tongji.tfor_backend.controller;
+
+import cn.edu.tongji.tfor_backend.repository.ZoneEntityRepository;
+import cn.edu.tongji.tfor_backend.service.ZoneInfoService;
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/view") //api mapping str
+@Api(tags = "apis for getting info about zone") // distribution for this series of api
+public class ZoneViewController {
+
+    @Autowired
+    ZoneInfoService zoneInfoService; // which service the controller used
+
+
+    @Operation(summary = "test api for zone") // distribution for single api
+    @GetMapping("getAllZone") // mapping url
+    public ResponseEntity<Object> getAllZoneNames() {
+        return new ResponseEntity<>(zoneInfoService.getAllZones(), HttpStatus.OK);
+    }
+
+
+
+}
---
+++ src/main/java/cn/edu/tongji/tfor_backend/repository/ZoneEntityRepository.java
@@ -0,0 +1,23 @@
+package cn.edu.tongji.tfor_backend.repository;
+
+import cn.edu.tongji.tfor_backend.model.ZoneEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ZoneEntityRepository extends JpaRepository<ZoneEntity, Integer>{
+
+
+
+
+    @Query("from ZoneEntity") // simplified sql of (select * from ZoneEntity)
+    List<ZoneEntity> findAllZones();
+
+
+    // interface temporarily of no use ()
+    public interface ZoneNameOnly {
+        public String getZoneName();
+    }
+
+}
