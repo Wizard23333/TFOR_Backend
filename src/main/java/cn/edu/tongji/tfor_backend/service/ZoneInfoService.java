e4ee2581a6a02eb483aed0f6cb37bd86e429b1b4
Haotian Wu <3233990645@qq.com>
2021-12-08 21:26:51 +0800

    simple demo for a api

---
+++ src/main/java/cn/edu/tongji/tfor_backend/service/ZoneInfoService.java
@@ -0,0 +1,13 @@
+package cn.edu.tongji.tfor_backend.service;
+
+
+import cn.edu.tongji.tfor_backend.model.ZoneEntity;
+
+import java.util.List;
+
+// this is a Service interface that can be implements
+public interface ZoneInfoService {
+
+    List<ZoneEntity> getAllZones();
+
+}
---
+++ src/main/java/cn/edu/tongji/tfor_backend/service/impl/ZoneInfoServiceImpl.java
@@ -0,0 +1,23 @@
+package cn.edu.tongji.tfor_backend.service.impl;
+
+import cn.edu.tongji.tfor_backend.model.ZoneEntity;
+import cn.edu.tongji.tfor_backend.repository.ZoneEntityRepository;
+import cn.edu.tongji.tfor_backend.service.ZoneInfoService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+
+// implements of the service interface
+@Service
+public class ZoneInfoServiceImpl implements ZoneInfoService {
+
+    @Resource
+    ZoneEntityRepository zoneEntityRepository;
+
+    @Override
+    public List<ZoneEntity> getAllZones() {
+        return zoneEntityRepository.findAllZones(); // call the repository layer's function
+    }
+}
