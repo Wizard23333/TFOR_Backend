bd08cbcbef65dd154ea280e4a0aa76b1ae86b96e
Wizard23333 <3233990645@qq.com>
2021-12-11 21:07:24 +0800

    rename a controller

--- src/main/java/cn/edu/tongji/tfor_backend/controller/ViewController.java
+++ src/main/java/cn/edu/tongji/tfor_backend/controller/ViewController.java
@@ -5,28 +5,28 @@ import cn.edu.tongji.tfor_backend.service.ZoneInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/view") //api mapping str
 @Api(tags = "apis for getting info about zone") // distribution for this series of api
-public class ZoneViewController {
+public class ViewController {
 
     @Autowired
     ZoneInfoService zoneInfoService; // which service the controller used
 
 
     @Operation(summary = "test api for zone") // distribution for single api
-    @GetMapping("getAllZone") // mapping url
+    @RequestMapping("getAllZone") // mapping url
     public ResponseEntity<Object> getAllZoneNames() {
         return new ResponseEntity<>(zoneInfoService.getAllZones(), HttpStatus.OK);
     }
+
 
 
 
