9e1fd7c4a162032785db494829b4b44f3b0ea65a
Wizard23333 <3233990645@qq.com>
2021-12-14 21:55:13 +0800

    recommend api

--- src/main/java/cn/edu/tongji/tfor_backend/controller/ViewController.java
+++ src/main/java/cn/edu/tongji/tfor_backend/controller/ViewController.java
@@ -1,16 +1,15 @@
 package cn.edu.tongji.tfor_backend.controller;
 
-import cn.edu.tongji.tfor_backend.repository.ZoneEntityRepository;
 import cn.edu.tongji.tfor_backend.service.ZoneInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/view") //api mapping str
@@ -21,9 +20,16 @@ public class ViewController {
     ZoneInfoService zoneInfoService; // which service the controller used
 
 
-    @Operation(summary = "test api for zone") // distribution for single api
-    @RequestMapping("getAllZone") // mapping url
+    @Operation(summary = "get all zones information") // distribution for single api
+    @GetMapping("getAllZone") // mapping url
     public ResponseEntity<Object> getAllZoneNames() {
         return new ResponseEntity<>(zoneInfoService.getAllZones(), HttpStatus.OK);
     }
+
+    @Operation(summary = "get personal recomandlist by userid")
+    @GetMapping("getRecommend/{userId}")
+    public ResponseEntity<Object> getRecommendListByUserId(@PathVariable("userId") Integer userId) {
+        return new ResponseEntity<>(zoneInfoService.getRecommend(userId), HttpStatus.OK);
+    }
+
 }
