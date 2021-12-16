17b89a7609732eab51121a7acdd0ef87293257fb
Wizard23333 <3233990645@qq.com>
2021-12-16 15:34:41 +0800

    zone apis

--- src/main/java/cn/edu/tongji/tfor_backend/repository/PostEntityRepository.java
+++ src/main/java/cn/edu/tongji/tfor_backend/repository/PostEntityRepository.java
@@ -5,7 +5,18 @@ import io.swagger.models.auth.In;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 public interface PostEntityRepository extends JpaRepository<PostEntity, Integer> {
 
     PostEntity findByContentId(Integer contentId);
+
+    // 根据时间区间和likenum查询前50条
+    List<PostEntity> findTop50ByLastEditTimeBetweenOrderByLikeNumDesc(Timestamp t1, Timestamp t2);
+
+//    PostEntity findTopByLastEditTimeBetweenOrderByLikeNumDesc(Timestamp t1, Timestamp t2);
+
+    List<PostEntity> findByPostTitleContainingOrTextContaining(String keyword1, String keyword2);
+
 }
