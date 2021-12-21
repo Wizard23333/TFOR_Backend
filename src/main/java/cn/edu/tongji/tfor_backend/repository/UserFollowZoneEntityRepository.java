6cd45f0d6f4c4d2690bde5fa217a1a730218c435
zb2313 <2224105295@qq.com>
2021-12-21 23:35:35 +0800

    delete apis

--- src/main/java/cn/edu/tongji/tfor_backend/repository/UserFollowZoneEntityRepository.java
+++ src/main/java/cn/edu/tongji/tfor_backend/repository/UserFollowZoneEntityRepository.java
@@ -4,10 +4,22 @@ import cn.edu.tongji.tfor_backend.model.UserFollowZoneEntity;
 import cn.edu.tongji.tfor_backend.model.UserFollowZoneEntityPK;
 import io.swagger.models.auth.In;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Propagation;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface UserFollowZoneEntityRepository extends JpaRepository<UserFollowZoneEntity, UserFollowZoneEntityPK> {
 
+    @Transactional
+    @Modifying
+    @Query(value ="delete from user_follow_zone where user_id=:userId and zone_id=:zoneId",nativeQuery = true)
+    void deleteByUserIdAndZoneId(@Param("userId") Integer userId,@Param("zoneId") Integer zoneId);
+
     List<UserFollowZoneEntity> findByUserId(Integer userId);
+
+
 }
