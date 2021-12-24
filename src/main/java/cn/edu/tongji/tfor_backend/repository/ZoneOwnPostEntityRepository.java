5d39a69e27c08a9cf0213c607abc1f09972971c8
haha <2210946752@qq.com>
2021-12-24 12:50:54 +0800

    Encapsulating HTTP requests  and add some APIs

--- src/main/java/cn/edu/tongji/tfor_backend/repository/ZoneOwnPostEntityRepository.java
+++ src/main/java/cn/edu/tongji/tfor_backend/repository/ZoneOwnPostEntityRepository.java
@@ -4,10 +4,17 @@ import cn.edu.tongji.tfor_backend.model.ZoneOwnPostEntity;
 import cn.edu.tongji.tfor_backend.model.ZoneOwnPostEntityPK;
 import io.swagger.models.auth.In;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ZoneOwnPostEntityRepository extends JpaRepository<ZoneOwnPostEntity, ZoneOwnPostEntityPK> {
-
     List<ZoneOwnPostEntity> findByZoneId(Integer zoneId);
+
+
+    @Query(value="delete z from zone_own_post z where z.content_id=postId", nativeQuery = true)
+    @Modifying
+    void deleteByPostId(@Param("postId") int postId);
 }
