c26ede54f28c087b1aa64e4f61fc9e9ddb3dfa35
haha <2210946752@qq.com>
2021-12-28 20:45:47 +0800

    Encapsulate Http requests

--- src/main/java/cn/edu/tongji/tfor_backend/controller/UserController.java
+++ src/main/java/cn/edu/tongji/tfor_backend/controller/UserController.java
@@ -1,5 +1,6 @@
 package cn.edu.tongji.tfor_backend.controller;
 
+import cn.edu.tongji.tfor_backend.configuration.HttpResponse;
 import cn.edu.tongji.tfor_backend.model.UserEntity;
 import cn.edu.tongji.tfor_backend.myannotation.Auth;
 import cn.edu.tongji.tfor_backend.service.EmailService;
@@ -36,56 +37,77 @@ public class UserController {
     //用户注册
     @Operation(summary = "api for user register") // distribution for single api
     @PostMapping("register") // mapping url
-    public void createUserByObject(@RequestBody UserEntity newUser) {
-        userInfoService.createUserByObject(newUser);
+    public HttpResponse createUserByObject(@RequestBody UserEntity newUser) {
+        try {
+            userInfoService.createUserByObject(newUser);
+        }
+        catch (Exception e) {
+            return HttpResponse.error(e.toString());
+        }
+        return HttpResponse.success("register successfully");
     }
 
     //用户使用密码登录
     //登录成功返回token，目前还未写好
     @Operation(summary = "INPUT PARAM:[uid],[pwd]")
     @PostMapping("login/pwd")
-    public ResponseEntity<JSONObject> loginByPwd(@RequestBody JSONObject jsonObject) {
+    public HttpResponse loginByPwd(@RequestBody JSONObject jsonObject) {
         int uid = Integer.parseInt(jsonObject.getString("uid"));
         String pwd = jsonObject.getString("pwd");
         JSONObject resp = new JSONObject();
-        if(userInfoService.loginByPwd(uid, pwd)){
-            resp.put("token",tokenService.createToken(String.valueOf(uid), 0));
-            return new ResponseEntity<>(resp, HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>(resp, HttpStatus.NO_CONTENT);
+        try {
+            if (userInfoService.loginByPwd(uid, pwd)) {
+                resp.put("token", tokenService.createToken(String.valueOf(uid), 0));
+                return HttpResponse.success(resp);
+            } else {
+                return HttpResponse.error("No content");
+            }
+        }
+        catch (Exception e) {
+            return HttpResponse.error(e.toString());
         }
     }
 
     //用户使用邮箱验证码登录
     @Operation(summary = "INPUT PARAM:[email],[verifyCode]")
     @PostMapping("login/email")
-    public ResponseEntity<JSONObject> loginByEmail(@RequestBody JSONObject jsonObject) {
+    public HttpResponse loginByEmail(@RequestBody JSONObject jsonObject) {
         String email = jsonObject.getString("email");
         String verifyCode = jsonObject.getString("verifyCode");
         JSONObject resp = new JSONObject();
         String emailVerifyCode = emailService.getEmailVerifyCode();
-        if (Objects.equals(verifyCode, emailVerifyCode)){
-            //通过email获取用户的id和密码，并将uid返回供前端使用
-            resp.put("token",tokenService.createToken(email, 1));
-            return new ResponseEntity<>(resp, HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>(resp, HttpStatus.NO_CONTENT);
+        try {
+            if (Objects.equals(verifyCode, emailVerifyCode)) {
+                //通过email获取用户的id和密码，并将uid返回供前端使用
+                resp.put("token", tokenService.createToken(email, 1));
+                return HttpResponse.success(resp);
+            } else {
+                return HttpResponse.error("No content");
+            }
+        }
+        catch(Exception e) {
+            return HttpResponse.error(e.toString());
         }
     }
 
     //用户使用短信验证码登录
     @Operation(summary = "INPUT PARAM:[tel],[verifyCode]")
     @PostMapping("login/tel")
-    public ResponseEntity<JSONObject> loginByTel(@RequestBody JSONObject jsonObject) {
+    public HttpResponse loginByTel(@RequestBody JSONObject jsonObject) {
         String tel = jsonObject.getString("tel");
         String verifyCode = jsonObject.getString("verifyCode");
         String telVerifyCode = telephoneService.getTelVerifyCode();
         JSONObject resp = new JSONObject();
-        if (Objects.equals(verifyCode, telVerifyCode)){
-            resp.put("token",tokenService.createToken(tel, 2));
-            return new ResponseEntity<>(resp, HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>(resp, HttpStatus.NO_CONTENT);
+        try{
+            if (Objects.equals(verifyCode, telVerifyCode)){
+                resp.put("token",tokenService.createToken(tel, 2));
+                return HttpResponse.success(resp);
+            }else{
+                return HttpResponse.error("No content");
+            }
+        }
+        catch (Exception e) {
+            return HttpResponse.error(e.toString());
         }
     }
 
@@ -93,53 +115,83 @@ public class UserController {
     //使用JSONObject作为形参更加灵活
     @Operation(summary = "INPUT PARAM:[uid],[userName],[userGender],[userImage].后面3个选项可选")
     @PutMapping("modify")
-    public void modifyUserInfo(@RequestBody JSONObject jsonObject){
+    public HttpResponse modifyUserInfo(@RequestBody JSONObject jsonObject){
         int uid = Integer.parseInt(jsonObject.getString("uid"));
         String userName = jsonObject.getString("userName");
         String userGender = jsonObject.getString("userGender");
         String userImage = jsonObject.getString("userImage");
-        userInfoService.changeUserName(uid, userName);
-        userInfoService.changeUserGender(uid, userGender);
-        userInfoService.changeUserImage(uid, userImage);
+        try {
+            userInfoService.changeUserName(uid, userName);
+            userInfoService.changeUserGender(uid, userGender);
+            userInfoService.changeUserImage(uid, userImage);
+            return HttpResponse.success();
+        }
+        catch (Exception e) {
+            return HttpResponse.error(e.toString());
+        }
     }
 
     //用户修改手机号信息
     @Operation(summary = "INPUT PARAM:[uid],[telNum],[verifyCode]")
     @PutMapping("changePhoneNbr")
-    public void changePhoneNbr(@RequestBody JSONObject jsonObject){
+    public HttpResponse changePhoneNbr(@RequestBody JSONObject jsonObject){
         int uid = Integer.parseInt(jsonObject.getString("uid"));
         String newTelNbr = jsonObject.getString("telNum");
         String verifyCode = jsonObject.getString("verifyCode");
-        String telVerifyCode = telephoneService.getTelVerifyCode();
-        if (Objects.equals(verifyCode, telVerifyCode)){
-            userInfoService.changePhoneNbr(uid, newTelNbr);
+        try {
+            String telVerifyCode = telephoneService.getTelVerifyCode();
+            if (Objects.equals(verifyCode, telVerifyCode)){
+                userInfoService.changePhoneNbr(uid, newTelNbr);
+            }
+            return HttpResponse.success();
+        }
+        catch (Exception e) {
+            return HttpResponse.error(e.toString());
         }
     }
 
     //用户修改邮箱号信息
     @Operation(summary = "INPUT PARAM:[uid],[email],[verifyCode]")
     @PutMapping("changeEmail")
-    public void changeEmail(@RequestBody JSONObject jsonObject){
+    public HttpResponse changeEmail(@RequestBody JSONObject jsonObject){
         int uid = Integer.parseInt(jsonObject.getString("uid"));
         String newEmail = jsonObject.getString("email");
         String verifyCode = jsonObject.getString("verifyCode");
-        String emailVerifyCode = emailService.getEmailVerifyCode();
-        if (Objects.equals(verifyCode, emailVerifyCode)) {
-            userInfoService.changeEmail(uid, newEmail);
+        try {
+            String emailVerifyCode = emailService.getEmailVerifyCode();
+            if (Objects.equals(verifyCode, emailVerifyCode)) {
+                userInfoService.changeEmail(uid, newEmail);
+            }
+            return HttpResponse.success();
+        }
+        catch (Exception e) {
+            return HttpResponse.error(e.toString());
         }
     }
 
     //发送邮箱验证码请求
     @Auth
     @GetMapping("getverifycode/email/{emailaddr}")
-    public void getEmailVerifyCode(@PathVariable String emailaddr){
-        emailService.sendEmailVerifyCode(emailaddr);
+    public HttpResponse getEmailVerifyCode(@PathVariable String emailaddr){
+        try{
+            emailService.sendEmailVerifyCode(emailaddr);
+            return HttpResponse.success();
+        }
+        catch (Exception e) {
+            return HttpResponse.error(e.toString());
+        }
     }
 
     //发送短信验证码请求
     @GetMapping("getverifycode/tel/{telnum}")
-    public void getTelVerifyCode(@PathVariable String telnum){
-        telephoneService.sendTelVerifyCode(telnum);
+    public HttpResponse getTelVerifyCode(@PathVariable String telnum){
+        try{
+            telephoneService.sendTelVerifyCode(telnum);
+            return HttpResponse.success();
+        }
+        catch (Exception e) {
+            return HttpResponse.error(e.toString());
+        }
     }
 
 }
