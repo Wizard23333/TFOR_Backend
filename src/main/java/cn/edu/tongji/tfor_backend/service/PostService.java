30c28c43fa79681c1addc3d121e76c33ac4d9059
haha <2210946752@qq.com>
2021-12-30 09:06:35 +0800

    add post exception

--- src/main/java/cn/edu/tongji/tfor_backend/service/PostService.java
+++ src/main/java/cn/edu/tongji/tfor_backend/service/PostService.java
@@ -1,5 +1,6 @@
 package cn.edu.tongji.tfor_backend.service;
 
+import cn.edu.tongji.tfor_backend.exceptionclass.PostException;
 import cn.edu.tongji.tfor_backend.model.AdvertisementEntity;
 import cn.edu.tongji.tfor_backend.model.CommentEntity;
 import cn.edu.tongji.tfor_backend.model.PostEntity;
@@ -9,10 +10,10 @@ import org.apache.coyote.Response;
 import java.util.List;
 
 public interface PostService {
-    int postContent(PostEntity postEntity);
+    int postContent(PostEntity postEntity) throws PostException;
     int enterZone(ZoneOwnPostEntity zoneOwnPostEntity);
     int postAdvertisement(AdvertisementEntity advertisementEntity);
-    int postComment(CommentEntity commentEntity);
+    int postComment(CommentEntity commentEntity) throws PostException;
     int deleteContent(String pid);
     int deleteAdvertisement(Integer aid);
     int deleteComment(String cid);
